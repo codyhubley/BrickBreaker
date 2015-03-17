203dd73700ba5a6a831ae723409c1c869d9f9dea
Cody <Cody@75.102.87.42>
2015-03-16 21:24:32 -0400

    Changes

--- BrickBricker/src/MainMenuPanel.java
+++ BrickBricker/src/MainMenuPanel.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JPanel;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +10,6 @@
  *
  * @author Cody
  */
-public class MainMenuPanel {
+public class MainMenuPanel extends JPanel {
     
 }

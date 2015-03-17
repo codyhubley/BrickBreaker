ec9c4a93749b88bb0329fa68c6186bc8d46eef66
Cody <Cody@75.102.87.42>
2015-03-16 21:24:07 -0400

    MainFrame set up

--- BrickBricker/src/MainFrame.java
+++ BrickBricker/src/MainFrame.java
@@ -1,3 +1,7 @@
+
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +11,14 @@
  *
  * @author Cody
  */
-public class MainFrame {
+public class MainFrame extends JFrame {
+    
+    public MainFrame(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false); 
+        setLocation(280,15);
+        setSize (950, 650);
+        setVisible(true);
+    }
     
 }

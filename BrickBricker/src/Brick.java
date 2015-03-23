7493fa125291de1349ef2ff16aa69e959e7a8497
SZG5293 <cwh5278@206-L03.up.ist.local>
2015-03-23 19:11:21 -0400

    Changes

--- BrickBricker/src/Brick.java
+++ BrickBricker/src/Brick.java
@@ -1,3 +1,8 @@
+
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +12,29 @@
  *
  * @author Cody
  */
+ 
 public class Brick {
+
+    
+JButton[] brick;
+int brickx[] = new int[45];
+int bricky[] = new int[45];
+int x = 15;
+int y = 30;
+
+  
+ for (int i = 0; i < Brick.length; i++) 
+ {
+ Brick[i] = new Rectangle(brickx, bricky, 30, 10);
+ if (i == 5) {
+ brickx = 70;
+ bricky = 62;
+   }
+   if (i == 9) {
+    brickx = 100;
+    bricky = 74;
+   }
+   brickx += 31;
+  }
     
 }

060669b7a7507ac24990b855a3733445e875242c
Cody <Cody@75.102.87.42>
2015-03-23 19:14:17 -0400

    main menu panel

--- BrickBricker/src/MainMenuPanel.java
+++ BrickBricker/src/MainMenuPanel.java
@@ -1,4 +1,8 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /*
@@ -10,6 +14,37 @@ import javax.swing.JPanel;
  *
  * @author Cody
  */
-public class MainMenuPanel extends JPanel {
+public class MainMenuPanel extends JPanel implements ActionListener{
+    
+    JButton play;
+    JButton instructions;
+    JButton exit;
+    JLabel title;
+    
+    MainMenuPanel(){
+        play = new JButton("Play");
+        instructions = new JButton ("Instructions");
+        exit = new JButton("Exit");
+        title = new JLabel ("Brick Breaker");
+        
+        add(title);
+        add(play);
+        add(instructions);
+        add(exit);
+        
+        play.addActionListener(this);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        Object obj = e.getSource();
+        GamePanel theGamePanel = new GamePanel();
+        if(obj == play) 
+          {
+            remove(this);
+            add(theGamePanel);
+            revalidate();
+            
+          }
+    }
     
 }

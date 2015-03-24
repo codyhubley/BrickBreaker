f71f2fe702ab9b5ea61d283734a2b59249400f29
cwh5278 <cwh5278@206-L03.up.ist.local>
2015-03-23 20:53:53 -0400

    Changes

--- BrickBricker/src/Brick.java
+++ BrickBricker/src/Brick.java
@@ -13,28 +13,8 @@ import javax.swing.JButton;
  * @author Cody
  */
  
-public class Brick {
-
-    
-JButton[] brick;
-int brickx[] = new int[45];
-int bricky[] = new int[45];
-int x = 15;
-int y = 30;
-
+public class Brick extends JButton {
   
- for (int i = 0; i < Brick.length; i++) 
- {
- Brick[i] = new Rectangle(brickx, bricky, 30, 10);
- if (i == 5) {
- brickx = 70;
- bricky = 62;
-   }
-   if (i == 9) {
-    brickx = 100;
-    bricky = 74;
-   }
-   brickx += 31;
-  }
-    
+int x = 15;
+int y = 30;    
 }
